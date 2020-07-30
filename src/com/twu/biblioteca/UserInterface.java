acec2c50e68f178f92fba1863a587457f16db920
Crazy-Ryan <ranwang1995@outlook.com>
2020-07-30 18:35:58 +0800

    [RanWang] feat: set up the 3-tier architecture of the application

---
+++ src/com/twu/biblioteca/UserInterface.java
@@ -0,0 +1,5 @@
+package com.twu.biblioteca;
+
+public class UserInterface {
+    private BookService bookService = new BookService();
+}
--- test/com/twu/biblioteca/BasicFuncTest.java
+++ test/com/twu/biblioteca/BasicFuncTest.java
@@ -1,13 +1,7 @@
 package com.twu.biblioteca;
 
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class ExampleTest {
-
-    @Test
-    public void test() {
-        assertEquals(1, 1);
-    }
+public class BasicFuncTest {
 }
