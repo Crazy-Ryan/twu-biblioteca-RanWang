a723c8d6af3c2d3fd16a149cb5138e7873cc0d91
Crazy-Ryan <ranwang1995@outlook.com>
2020-07-30 23:30:29 +0800

    [RanWang] feat1.3: display option menu and accept selection of option

--- src/com/twu/biblioteca/UserInterface.java
+++ src/com/twu/biblioteca/UserInterface.java
@@ -1,13 +1,14 @@
 package com.twu.biblioteca;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class UserInterface {
     private BookService bookService = new BookService();
 
     public void run() {
         greet();
-        viewBookList();
+        selectOptionOnMenu();
     }
 
     protected void greet() {
@@ -19,6 +20,20 @@ public class UserInterface {
         System.out.print("Title\t| Author\t| Publication Year\n");
         for (Book book : bookData) {
             System.out.print(book.getTitle() + "\t| " + book.getAuthor() + "\t| " + book.getPublicationYear() + "\n");
+        }
+    }
+
+    protected void selectOptionOnMenu() {
+        System.out.print("Please select an option\n" +
+                "1. List of books\n");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                viewBookList();
+                break;
+            default:
+                System.out.println("unavailable");
         }
     }
 }
